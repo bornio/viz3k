a4c6b475d9eb8f7888260c05dc6d2c86110c6618
Scott Bai <scott.bai@gmail.com>
2012-10-05 19:50:04 -0400

    Cosmetic changes to force.css and force.js.
    Changed text to have white stroke/outline around it for visibility against dark backgrounds. Changed force.js so text always appears above circles in graph. Changed force.js so circle and text size are both scaled relative to the node with the largest number of links. That is, the largest circle in one graph will always be the same size as the largest circle in another graph, even if the absolute number of links to each respective node are different between the graphs. This is to avoid ginormous circles in the full-novel graph and invisibly small ones in per-chapter graphs.

--- coappear/force.js
+++ coappear/force.js
@@ -1,6 +1,6 @@
 function coappear(data_file_path)
 {
-  var width = 1280, height = 1280;
+  var width = 1280, height = 1024;
   var force = d3.layout.force().charge(-360).linkDistance(200).size([width, height]);
   var svg = d3.select("#chart").append("svg").attr("width", width).attr("height", height);
 
@@ -23,16 +23,29 @@ function coappear(data_file_path)
         .attr("class", "node")
         .call(force.drag);
 
-    node.append("circle")
+    var circle = node.append("circle")
         .attr("class", "node")
-        .attr("r", function(d) { return 3*Math.log(d.links); })
+
+    var text = svg.append("svg:g").selectAll("g")
+        .data(force.nodes())
+        .enter().append("svg:g");
+
+    // scale all circles and text labels relative to most heavily-linked node
+    var max_links = 0.0;
+    text.each(function(d) { if (d.links > max_links) { max_links = d.links; } })
+
+    circle
+        .attr("r", function(d) { return 20*Math.sqrt(d.links/max_links) + 2; })
         .style("fill", function(d) { return d3.rgb(d.color); })
 
-    node.append("text")
+    text.append("svg:text")
         .attr("dx", 0)
         .attr("dy", 0)
+        .attr("text-anchor", "middle")
+        .attr("dominant-baseline", "central")
         .text(function(d) { return d.name })
-        .attr("font-size", function(d) { return (String(0.2*d.links + 5) + "px")});
+        .attr("font-size", function(d) { return (String(20*(d.links/max_links) + 8) + "px")})
+        .attr("stroke-width", function(d) { return (String(d.links/max_links) + "px")});
 
     force.on("tick", function()
     {
@@ -42,6 +55,7 @@ function coappear(data_file_path)
           .attr("y2", function(d) { return d.target.y; });
 
       node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      text.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
     });
   });
 }
