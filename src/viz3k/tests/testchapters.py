2dc4169074980bc3249a3af74ab952c69796f77e
Scott Bai <scott.bai@gmail.com>
2012-12-08 16:08:35 -0500

    Add testchapters.py to test correctness of chapters.json.

---
+++ src/viz3k/tests/testchapters.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from testify import *
+
+import data.chapters
+
+class TestChapters(TestCase):
+    '''Tests for correctness of chapters.json.'''
+    @setup
+    def setup(self):
+        # load the chapters.json data file
+        self.chapters = data.chapters.from_json("../../website/data/chapters.json")
+
+    def test_unique_chapter_numbers(self):
+        '''Each chapter should have a unique chapter number'''
+        chapter_nums = []
+        for chapter in self.chapters:
+            assert_not_in(chapter.chapter, chapter_nums, "Chapter number " + str(chapter.chapter) + " is not unique")
+            chapter_nums.append(chapter.chapter)
+
+    def test_contiguous_chapter_numbers(self):
+        '''When sorted, the list of chapter numbers should be {1,2,...,num_chapters}.'''
+        chapter_nums = []
+        for chapter in self.chapters:
+            chapter_nums.append(chapter.chapter)
+
+        chapter_nums.sort()
+        for i in range(1,len(chapter_nums)+1):
+            assert_equal(chapter_nums[i - 1],i)
+
+    def test_unique_page_numbers(self):
+        '''Each page should have a unique page number'''
+        page_nums = []
+        for chapter in self.chapters:
+            for page in chapter.pages:
+                assert_not_in(page.page, page_nums, "Page number " + str(page.page) + " is not unique")
+                page_nums.append(page.page)
+
+if __name__ == "__main__":
+    run()
