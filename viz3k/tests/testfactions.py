811e2a015cd69b399cc019d85b8e49a74fa04177
Scott Bai <scott.bai@gmail.com>
2013-01-13 19:11:20 -0500

    Add tests for factions.json.

---
+++ viz3k/tests/testfactions.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from testify import *
+
+import data.factions
+
+class TestFactions(TestCase):
+    '''Tests for correctness of factions.json.'''
+    @setup
+    def setup(self):
+        # load the factions.json data file
+        self.factions = data.factions.from_json("../data/factions.json")
+
+    def test_ids(self):
+        '''Each faction should have a unique id'''
+        ids = []
+        for faction in self.factions:
+            assert_not_in(faction.id, ids, "Id " + str(faction.id) + " is not unique")
+            ids.append(faction.id)
+
+    def test_names(self):
+        '''Each faction should have a unique name'''
+        names = []
+        for faction in self.factions:
+            assert_not_in(faction.name, names, "Name " + faction.name + " is not unique")
+            names.append(faction.name)
+
+if __name__ == "__main__":
+    run()
