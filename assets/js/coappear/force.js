bornio/viz3k

commit 9460274ed9963b8a5df9c7626ecfff411790fc19

Coappearances: sort character list sidebar by number of links.
- Factor sorting code out into a function.

assets/js/coappear/force.js
@@ -134,8 +134,10 @@ function appear_force(data_nodes, data_links, coappear_ids)
   };
 }
 
-function appear_stack(characters, graph)
+function appear_stack(nodes, graph)
 {
+  var characters = nodes_sort_by_links(nodes);
+
   // style
   var bar_height = 22;
   var bar_width = 24;
@@ -275,4 +277,29 @@ function coappear(data_file_path, data_callback)
 
     window.addEventListener("resize", coappear_resize, false);
   });
+}
+
+function nodes_sort_by_links(nodes)
+{
+  // sort the characters by decreasing number of links
+  var sorted = nodes.slice();
+  sorted.sort(function(a,b)
+  {
+    // use alphabetical name sorting as tie-breaker
+    if (b.links == a.links)
+    {
+      if (a.name < b.name)
+      {
+        return -1;
+      }
+      else if (a.name > b.name)
+      {
+        return 1;
+      }
+      return 0;          
+    }
+
+    return b.links - a.links;
+  });
+  return sorted;
 }

assets/js/coappear/chapter.js
@@ -48,24 +48,7 @@ function Chapter($scope, $http)
   // callback to get data back from asynchronous load
   var compute_stats = function(nodes, links) {
     // calculate importance by sorting nodes by decreasing number of links
-    nodes.sort(function(a,b)
-      {
-        // use alphabetical name sorting as tie-breaker
-        if (b.links == a.links)
-        {
-          if (a.name < b.name)
-          {
-            return -1;
-          }
-          else if (a.name > b.name)
-          {
-            return 1;
-          }
-          return 0;          
-        }
-
-        return b.links - a.links;
-      });
+    nodes = nodes_sort_by_links(nodes);
 
     // get the top five
     $scope.people_by_importance = nodes.slice(0,5);

commit 7b382d24791b0805c015e275fa1207aab6f65187

Fix incorrect indices after sorting character list.
Bug was introduced in 9460274ed9963b8a5df9c7626ecfff411790fc19

assets/js/coappear/force.js
@@ -138,6 +138,19 @@ function appear_stack(nodes, graph)
 {
   var characters = nodes_sort_by_links(nodes);
 
+  // map new (sorted) indices back to the ones used by D3 to draw the graph
+  //var indices = new Array(nodes.length);
+  for (c = 0; c < characters.length; c++)
+  {
+    for (n = 0; n < nodes.length; n++)
+    {
+      if (characters[c].id == nodes[n].id)
+      {
+        characters[c].node_index = n;
+      }
+    }
+  }
+
   // style
   var bar_height = 22;
   var bar_width = 24;
@@ -185,25 +198,25 @@ function appear_stack(nodes, graph)
     .attr("font-size", label_size);
 
   // set mouseover and mouseout events for each bar area and the bars themselves
-  function stack_mouseover(node, node_index)
+  function stack_mouseover(character, character_index)
   {
     // highlight this person's bar in the stack
-    bar_areas.filter(function(d) { return (d.id == node.id) ? this : null; })
+    bar_areas.filter(function(d) { return (d.id == character.id) ? this : null; })
       .style("fill", "#aaeeff")
       .style("opacity", 1.0);
 
     // highlight this person's circle in the graph
-    graph.highlight_i(node, node_index);
+    graph.highlight_i(character, character.node_index);
   }
 
-  function stack_mouseout(node, node_index)
+  function stack_mouseout(character, character_index)
   {
     // unhighlight this person's bar in the stack
-    bar_areas.filter(function(d) { return (d.id == node.id) ? this : null; })
+    bar_areas.filter(function(d) { return (d.id == character.id) ? this : null; })
       .style("opacity", 0.0);
 
     // unhighlight this person's circle in the graph
-    graph.highlight_o(node, node_index);
+    graph.highlight_o(character, character.node_index);
   }
 
   stack_items.on("mouseover", function(d, i) { stack_mouseover(d, i); });