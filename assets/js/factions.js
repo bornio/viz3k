b6b725ce7ebf1399470144e47f3946412257e5b6
Scott Bai <scott.bai@gmail.com>
2013-01-13 23:11:07 -0500

    Factions: presort factions list by size, tie-broken by name.

--- assets/js/factions.js
+++ assets/js/factions.js
@@ -7,14 +7,42 @@ function Factions($scope, $http)
       $scope.factions = data.factions;
 
       // keep the "Other" faction (id = 99) separate
-      for (var i = 0; i < data.factions.length; i++)
+      for (var i = 0; i < $scope.factions.length; i++)
       {
-        if (data.factions[i].id == 99)
+        if ($scope.factions[i].id == 99)
         {
-          $scope.factions_other = [data.factions[i]];
+          $scope.factions_other = [$scope.factions[i]];
           $scope.factions.splice(i,1);
         }
       }
+
+      // sort the factions
+      $scope.factions = factions_sort_by_size($scope.factions);
     }
   );
+}
+
+function factions_sort_by_size(factions)
+{
+  var sorted = factions.slice();
+  sorted.sort(function(a,b)
+  {
+    // use alphabetical name sorting as tie-breaker
+    if (b.size == a.size)
+    {
+      if (a.name < b.name)
+      {
+        return -1;
+      }
+      else if (a.name > b.name)
+      {
+        return 1;
+      }
+      return 0;          
+    }
+
+    return b.size - a.size;
+  });
+
+  return sorted;
 }
