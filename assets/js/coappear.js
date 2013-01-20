b8acaf4415df14cf59ee23101b25896ad2d9230d
Scott Bai <scott.bai@gmail.com>
2013-01-19 22:02:50 -0500

    Make coappear.html more consistent with other pages.
    - Make table scrollable with 600px height and use table-condensed instead of table-striped.

--- assets/js/coappear.js
+++ assets/js/coappear.js
@@ -3,14 +3,14 @@ function ChapterList($scope, $http)
   // navbar settings
   $scope.navbar_url = "/navbar";
   $scope.navbar_selected = 1;
+
+  var populate_chapters = function(data)
+  {
+    $scope.chapters = data.chapters;
+  }
   
   // issue an http get to grab the data file
-  $http.get("/data/chapters").success(
-    function(data)
-    {
-      $scope.chapters = data.chapters;
-    }
-  );
+  $http.get("/data/chapters").success(populate_chapters);
 
   // default ordering of chapters list is by chapter number
   $scope.orderProp = "chapter";
