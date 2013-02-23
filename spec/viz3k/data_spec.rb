63f1cf6ab7608fa6422a48b0f27fc5e3330cbf60
Scott Bai <scott.bai@gmail.com>
2013-02-23 14:11:26 -0500

    Add tests for allegiances.json data sanity.
    Fix allegiance info for Feng Xu.

--- spec/viz3k/data_spec.rb
+++ spec/viz3k/data_spec.rb
@@ -2,7 +2,9 @@
 # encoding: utf-8
 
 require 'spec_helper'
-require 'data'
+require "chapters"
+require "factions"
+require "people"
 require 'open-uri'
 
 describe "data" do
@@ -43,42 +45,98 @@ describe "data" do
     end
   end
 
-  describe "characters.json" do
+  describe "characters.json and allegiances.json" do
     before(:all) do
-      @people = Viz3k::People.new(@data_path + "/characters.json")
+      @people = Viz3k::People.new(@data_path + "/characters.json", @data_path + "/allegiances.json")
     end
 
-    it "should not contain any people with the same id" do
-      ids = []
-      @people.people.each do |person|
-        ids.should_not include(person.id)
-        ids.push(person.id)
-      end
-    end
-
-    it "should not contain more than one person with the same name" do
-      names = []
-      @people.people.each do |person|
-        names.should_not include(person.name)
-        names.push(person.name)
-      end
-    end
-
-    it "should have only contiguous ids starting from 0, i.e. {0,1,2,...,num_ids-1}" do
-      ids = @people.people.map{|person| person.id}
-      ids.sort!()
-      ids.each do |id|
-        ids.index(id).should == id
-      end
-    end
-
-    describe "external links" do
-      it "should not have any broken links to wikipedia", :network => true, :speed => "slow" do
+    describe "characters.json" do
+      it "should not contain any people with the same id" do
+        ids = []
         @people.people.each do |person|
-          if (person.wiki != "")
-            uri = open(URI.encode(person.wiki))
-            uri.status[1].should == "OK"
+          ids.should_not include(person.id)
+          ids.push(person.id)
+        end
+      end
+
+      it "should not contain more than one person with the same name" do
+        names = []
+        @people.people.each do |person|
+          names.should_not include(person.name)
+          names.push(person.name)
+        end
+      end
+
+      it "should have only contiguous ids starting from 0, i.e. {0,1,2,...,num_ids-1}" do
+        ids = @people.people.map{|person| person.id}
+        ids.sort!()
+        ids.each do |id|
+          ids.index(id).should == id
+        end
+      end
+
+      describe "external links" do
+        it "should not have any broken links to wikipedia", :network => true, :speed => "slow" do
+          @people.people.each do |person|
+            if (person.wiki != "")
+              uri = open(URI.encode(person.wiki))
+              uri.status[1].should == "OK"
+            end
           end
+        end
+      end
+    end
+
+    describe "allegiances.json" do
+      it "should contain an allegiance entry for each person in characters.json" do
+        @people.people.each do |person|
+          @people.allegiances.has_key?(person.id).should == true
+        end
+      end
+
+      it "should have at least one faction allegiance listed per person id" do
+        @people.people.each do |person|
+          person_allegiances = @people.allegiances[person.id]
+          person_allegiances.length().should > 0
+        end
+      end
+
+      it "should, when a person has > 1 factions listed, strictly order them by chron" do
+        @people.people.each do |person|
+          person_allegiances = @people.allegiances[person.id]
+          if (person_allegiances.length() > 1)
+            person_allegiances.each_with_index do |person_allegiance, i|
+              person_allegiance.chron.should == i
+            end
+          end
+        end
+      end
+
+      it "should cover the entire page range of the novel for every person" do
+        @people.people.each do |person|
+          person_allegiances = @people.allegiances[person.id]
+
+          # first interval should start at page 5
+          person_allegiances[0].interval[0].should == 5
+
+          combined_start = 5
+          combined_end = 5
+          person_allegiances.each do |person_allegiance|
+            interval_start = person_allegiance.interval[0]
+            interval_end = person_allegiance.interval[1]
+            
+            # the interval bounds should be defined in increasing page order
+            interval_end.should >= interval_start
+
+            # each interval should start exactly at the end of the previous interval
+            interval_start.should == combined_end
+
+            # merge the interval into the range we have so far
+            combined_end = interval_end
+          end
+
+          # last interval should end at page 1457
+          combined_end.should == 1457
         end
       end
     end
@@ -118,7 +176,7 @@ describe "data" do
 
   describe "people" do
     before(:all) do
-      @people = Viz3k::People.new(@data_path + "/characters.json")
+      @people = Viz3k::People.new(@data_path + "/characters.json", @data_path + "/allegiances.json")
       @chapters = Viz3k::Chapters.new(@data_path + "/chapters.json")
     end
 
