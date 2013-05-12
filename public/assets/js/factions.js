6549459311b270a32b5d4e0420110afd909b1833
Scott Bai <scott.bai@gmail.com>
2013-05-11 21:27:24 -0400

    Use $q.all instead of having callbacks within callbacks.

--- public/assets/js/factions.js
+++ public/assets/js/factions.js
@@ -1,68 +1,65 @@
-function FactionsController($scope, $http) {
+function FactionsController($scope, $http, $q) {
   // navbar settings
   $scope.navbarUrl = "/navbar";
   $scope.navbar_selected = 2;
 
   set_resize_handler_for("content-area", false);
 
-  var populateFactions = function(factionsData) {
-    var factions = factionsData.factions;
-    var factionsOther;
+  // get our data from the backend
+  var promises = [$http.get("/data/factions"), $http.get("/data/chapters")];
+  $q.all(promises).then(function(results) {
+    var factions = results[0].data.factions;
+    var chapters = results[1].data.chapters;
 
-    for (var i = 0; i < factions.length; i++) {
-      // for non-generic faction types, label the type in parentheses
-      label_faction_type(factions[i]);
+    // we have all the data we need
+    renderView($scope, factions, chapters);
+  });
+}
 
-      // keep the "Other" faction (id = 99) separate
-      if (factions[i].id == 99) {
-        factionsOther = [factions[i]];
-        factions.splice(i,1);
-      }
+function renderView($scope, factions, chapters) {
+  var factionsOther;
+
+  for (var i = 0; i < factions.length; i++) {
+    // for non-generic faction types, label the type in parentheses
+    label_faction_type(factions[i]);
+
+    // keep the "Other" faction (id = 99) separate
+    if (factions[i].id == 99) {
+      factionsOther = [factions[i]];
+      factions.splice(i,1);
     }
-
-    // sort the factions
-    factions = sortFactionsBySize(factions);
-
-    // add timeline showing appearances for all factions
-    var populateTimeline = function(chaptersData)
-    {
-      var chapters = chaptersData.chapters;
-      var maxPeople = 0;
-
-      // see what the max number of distinct characters to appear in any chapter is
-      for (var c = 0; c < chapters.length; c++) {
-        if (chapters[c].people.length > maxPeople) {
-          maxPeople = chapters[c].people.length;
-        }
-      }
-
-      // for each faction...
-      for (var f = 0; f < factions.length; f++) {
-        var faction = factions[f];
-        faction.chapters = new Array(chapters.length);
-
-        // find out how many of its members turn up in each chapter
-        for (var c = 0; c < chapters.length; c++) {
-          var chapter = chapters[c];
-          faction.chapters[c] = count_in_faction(faction, chapter);
-        }
-      }
-      
-      // display a stacked bar chart of faction appearances per chapter
-      var chart = chartAppearanceTimeline("chart-appearances", factions, chapters, maxPeople);
-      window.addEventListener("resize", chart.resized, false);
-
-      // assign data to the scope
-      $scope.factions = factions;
-      $scope.factionsOther = factionsOther;
-    }
-
-    // issue an http get to grab the data file
-    $http.get("/data/chapters").success(populateTimeline);
   }
 
-  // issue an http get to grab the data file
-  $http.get("/data/factions").success(populateFactions);
+  // sort the factions
+  factions = sortFactionsBySize(factions);
+
+  // see what the max number of distinct characters to appear in any chapter is
+  var maxPeople = 0;
+  for (var c = 0; c < chapters.length; c++) {
+    if (chapters[c].people.length > maxPeople) {
+      maxPeople = chapters[c].people.length;
+    }
+  }
+
+  // for each faction...
+  for (var f = 0; f < factions.length; f++) {
+    var faction = factions[f];
+    faction.chapters = new Array(chapters.length);
+
+    // find out how many of its members turn up in each chapter
+    for (var c = 0; c < chapters.length; c++) {
+      var chapter = chapters[c];
+      faction.chapters[c] = count_in_faction(faction, chapter);
+    }
+  }
+
+  // display a stacked bar chart of faction appearances per chapter
+  var chart = chartAppearanceTimeline("chart-appearances", factions, chapters, maxPeople);
+  window.addEventListener("resize", chart.resized, false);
+
+  // assign data to the scope
+  $scope.factions = factions;
+  $scope.factionsOther = factionsOther;
 }
 
 function sortFactionsBySize(factions) {
