c22a5b6dd008d657a07ed05774b1cc0baeabf39d
Scott Bai <scott.bai@gmail.com>
2013-05-13 19:35:48 -0400

    Break into two methods.

--- public/assets/js/util.js
+++ public/assets/js/util.js
@@ -1,44 +1,41 @@
 // count faction members only if they actually appear as an affiliate of this faction in the selected chapter
 function countFactionMembersInChapter(faction, chapter) {
-  var member_count = 0;
-  for (var m = 0; m < faction.members.length; m++)
-  {
+  var memberCount = 0;
+  for (var m = 0; m < faction.members.length; m++) {
     var member = faction.members[m];
-    var found_in_chapter = false;
+    var foundInChapter = false;
 
-    if (chapter.people.indexOf(member.id) >= 0)
-    {
-      // see if any of this member's intervals match this faction
-      for (var i = 0; i < member.allegiance.length; i++)
-      {
-        var affiliation = member.allegiance[i];
-        if (affiliation.faction == faction.id)
-        {
-          var interval_start = affiliation.interval[0];
-          var interval_end = affiliation.interval[1];
-          var chapter_start = chapter.pages[0];
-          var chapter_end = chapter.pages[chapter.pages.length - 1];
-
-          // see if this interval covers any part of the chapter
-          if ((interval_start >= chapter_start && interval_start <= chapter_end) ||
-              (interval_end >= chapter_start && interval_end <= chapter_end) ||
-              (interval_start <= chapter_start && interval_end >= chapter_end))
-          {
-            // if so, then count this character as appearing in this chapter as a member of this faction
-            found_in_chapter = true;
-            break;
-          }
-        }
+    if (chapter.people.indexOf(member.id) >= 0) {
+      // count this person if they are affiliated with this faction in this chapter
+      if (isFactionMemberInChapter(member, faction, chapter)) {
+        memberCount++;
       }
-    }
-
-    if (found_in_chapter)
-    {
-      member_count++;
     }
   }
 
-  return member_count;
+  return memberCount;
+}
+
+function isFactionMemberInChapter(person, faction, chapter) {
+  for (var i = 0; i < person.allegiance.length; i++) {
+    var affiliation = person.allegiance[i];
+    if (affiliation.faction == faction.id) {
+      var intervalStart = affiliation.interval[0];
+      var intervalEnd = affiliation.interval[1];
+      var chapterStart = chapter.pages[0];
+      var chapterEnd = chapter.pages[chapter.pages.length - 1];
+
+      // see if this interval covers any part of the chapter
+      if ((intervalStart >= chapterStart && intervalStart <= chapterEnd) ||
+          (intervalEnd >= chapterStart && intervalEnd <= chapterEnd) ||
+          (intervalStart <= chapterStart && intervalEnd >= chapterEnd)) {
+        // if so, then this character is a member of this faction during this chapter
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 // find and return the person with the given id
