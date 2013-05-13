91d6a297e0f39e09802651e009eae032e38e46ae
Scott Bai <scott.bai@gmail.com>
2013-05-13 19:07:26 -0400

    Use JavaScript conventions and promises.

--- public/assets/js/chapters.js
+++ public/assets/js/chapters.js
@@ -1,17 +1,18 @@
-function ChapterList($scope, $http)
-{
+function ChaptersController($scope, $http) {
   // navbar settings
-  $scope.navbar_url = "/navbar";
+  $scope.navbarUrl = "/navbar";
   $scope.navbar_selected = 1;
-
-  var populate_chapters = function(data)
-  {
-    $scope.chapters = data.chapters;
-  }
-  
-  // issue an http get to grab the data file
-  $http.get("/data/chapters").success(populate_chapters);
 
   // default ordering of chapters list is by chapter number
   $scope.orderProp = "chapter";
+
+  // issue an http get to grab the data file
+  var promise = $http.get("/data/chapters");
+  promise.then(function(results) {
+    renderView($scope, results.data.chapters);
+  });
+}
+
+var renderView = function($scope, chapters) {
+  $scope.chapters = chapters;
 }
