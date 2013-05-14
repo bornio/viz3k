805942f13c08bad117da235b3326db685e5a7150
Scott Bai <scott.bai@gmail.com>
2013-05-14 19:45:25 -0400

    Use JavaScript conventions.

--- public/assets/js/people/person.js
+++ public/assets/js/people/person.js
@@ -1,142 +1,123 @@
 // Controller for person.html.
-function Person($scope, $http)
-{
+function Person($scope, $http) {
   // use the number at the end of the URL to determine which person's data to load
-  person_id = document.URL.split("/").pop();
+  personId = document.URL.split("/").pop();
 
   // navbar settings
-  $scope.navbar_url = "/navbar";
+  $scope.navbarUrl = "/navbar";
   $scope.navbar_selected = 3;
 
   // initial values
-  $scope.style_parens = "";
+  $scope.styleParens = "";
   $scope.died = false;
-  $scope.has_killers = false;
+  $scope.hasKillers = false;
   $scope.kills = new Array();
-  $scope.kills_combat = new Array();
-  $scope.kills_murder = new Array();
-  $scope.kills_execution = new Array();
+  $scope.killsCombat = new Array();
+  $scope.killsMurder = new Array();
+  $scope.killsExecution = new Array();
 
   setResizeHandlerFor("content-area", false);
 
   // issue an http get to grab the info for all people
-  $http.get("/data/people").success(populate_person_info($scope, $http));
+  $http.get("/data/people").success(populatePersonInfo($scope, $http));
 }
 
-var populate_person_info = function($scope, $http, people_json)
-{
-  style_name_popover_title = "About style names"
-  style_name_popover_content = "A <em><a href='http://en.wikipedia.org/wiki/Chinese_style_name'>style name</a></em>, or <em>courtesy name</em>, is a name traditionally given to Chinese males when they reach adulthood. Characters in the novel frequently address one another by their style names instead of their given names as a mark of respect.";
-  return function(people_json)
-  {
-    var person = getPerson(people_json.people, person_id);
+var populatePersonInfo = function($scope, $http, peopleJson) {
+  styleNamePopoverTitle = "About style names"
+  styleNamePopoverContent = "A <em><a href='http://en.wikipedia.org/wiki/Chinese_style_name'>style name</a></em>, or <em>courtesy name</em>, is a name traditionally given to Chinese males when they reach adulthood. Characters in the novel frequently address one another by their style names instead of their given names as a mark of respect.";
+  return function(peopleJson) {
+    var person = getPerson(peopleJson.people, personId);
 
     // add parentheses to this person's style name
     personStyleParens(person);
 
     // add external links (if any) to a list
     person.links = [];
-    if ("wiki" in person)
-    {
-      person.links.push({text:"wiki",href:person.wiki});
+    if ("wiki" in person) {
+      person.links.push({ text:"wiki", href:person.wiki });
     }
-    if ("km" in person)
-    {
-      person.links.push({text:"km",href:person.km});
+    if ("km" in person) {
+      person.links.push({ text:"km", href:person.km });
     }
 
     // death information
-    if ("death" in person)
-    {
+    if ("death" in person) {
       death = person.death;
       $scope.died = true;
-      if ("killers" in death)
-      {
-        $scope.has_killers = true;
-        $scope.killers = killers_info(people_json.people, death.killers);
+      if ("killers" in death) {
+        $scope.hasKillers = true;
+        $scope.killers = killersInfo(peopleJson.people, death.killers);
       }
     }
 
     $scope.person = person;
     document.title = person.name + " " + person.style_parens + " - Viz3k";
 
-    if ("style_parens" in person)
-    {
-      $scope.style_parens = person.style_parens;
+    if ("style_parens" in person) {
+      $scope.styleParens = person.style_parens;
 
       // set a popover to explain style names
-      $('#style-name').popover({ title: style_name_popover_title,
-                                 content : style_name_popover_content });
+      $('#style-name').popover({ title: styleNamePopoverTitle, content : styleNamePopoverContent });
     }
 
     // issue an http get to grab the faction info
-    $http.get("/data/factions").success(populate_factions_info($scope, person));
+    $http.get("/data/factions").success(populateFactionsInfo($scope, person));
 
     // get info for people killed by this person
-    $http.get("/data/deaths/killed-by/" + person_id).success(populate_kills_info($scope, person_id));
+    $http.get("/data/deaths/killed-by/" + personId).success(populateKillsInfo($scope, personId));
   }
 }
 
-function populate_factions_info($scope, person, factions_json)
-{
-  return function(factions_json)
-  {
+function populateFactionsInfo($scope, person, factionsJson) {
+  return function(factionsJson) {
     // find this person's faction info
-    var primary_faction;
-    var other_factions = new Array();
-    for (var f in factions_json.factions)
+    var primaryFaction;
+    var otherFactions = new Array();
+    for (var f in factionsJson.factions)
     {
-      var faction = factions_json.factions[f];
+      var faction = factionsJson.factions[f];
 
       // primary faction
-      if (faction.id == person.faction)
-      {
-        primary_faction = faction;
-        labelFactionType(primary_faction);
+      if (faction.id == person.faction) {
+        primaryFaction = faction;
+        labelFactionType(primaryFaction);
       }
 
       // other affiliations
-      for (var a in person.allegiance)
-      {
+      for (var a in person.allegiance) {
         var affiliation = person.allegiance[a];
         if ((affiliation.faction != person.faction) &&
-            (affiliation.faction == faction.id))
-        {
+            (affiliation.faction == faction.id)) {
           labelFactionType(faction);
-          other_factions.push(faction);
+          otherFactions.push(faction);
         }
       }
     }
 
     // assign data to the scope
-    $scope.primary_faction = primary_faction;
-    $scope.other_factions = other_factions;
+    $scope.primaryFaction = primaryFaction;
+    $scope.otherFactions = otherFactions;
   }
 }
 
-function populate_kills_info($scope, person_id, kills_json)
-{
-  return function(kills_json)
-  {
-    var kills = kills_json.killed_by;
-    if (kills.length > 0)
-    {
+function populateKillsInfo($scope, personId, killsJson) {
+  return function(killsJson) {
+    var kills = killsJson.killed_by;
+    if (kills.length > 0) {
       $scope.kills = kills;
     }
 
-    $scope.kills_combat = killsOfType(kills, "combat");
-    $scope.kills_murder = killsOfType(kills, "murder");
-    $scope.kills_execution = killsOfType(kills, "execution");
+    $scope.killsCombat = killsOfType(kills, "combat");
+    $scope.killsMurder = killsOfType(kills, "murder");
+    $scope.killsExecution = killsOfType(kills, "execution");
   }
 }
 
-function killers_info(people, killers)
-{
+function killersInfo(people, killers) {
   var info = new Array();
-  for (k in killers)
-  {
-    var killer_info = getPerson(people, killers[k]);
-    info.push(killer_info);
+  for (var k in killers) {
+    var killerInfo = getPerson(people, killers[k]);
+    info.push(killerInfo);
   }
 
   return info;
