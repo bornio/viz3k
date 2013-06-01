dfb16bc1688a523fd2ae51f9da2b81ddde7f3212
Scott Bai <scott.bai@gmail.com>
2013-06-01 14:12:27 -0400

    Reorder examples.

--- spec/javascripts/charts/chartBarHorizontalSpec.js
+++ spec/javascripts/charts/chartBarHorizontalSpec.js
@@ -24,33 +24,6 @@ describe("chartBarHorizontal", function() {
     });
   });
 
-  describe("configure()", function() {
-    it("configures the chart to show value labels and axis ticks", function() {
-      var nvChart = chart.configure();
-      expect(nvChart.showValues()).toEqual(true);
-      expect(nvChart.xAxis.tickValues()).toEqual(null); // if null, the D3 axis generates its own tick values
-      expect(nvChart.yAxis.tickValues()).toEqual(null); // if null, the D3 axis generates its own tick values
-    });
-
-    it("hides x axis ticks if showXAxis(false) was called", function() {
-      var nvChart = chart.showXAxis(false).configure();
-      expect(nvChart.xAxis.tickValues()).toEqual([]);
-      expect(nvChart.yAxis.tickValues()).toEqual(null);
-    });
-
-    it("hides y axis ticks if showYAxis(false) was called", function() {
-      var nvChart = chart.showYAxis(false).configure();
-      expect(nvChart.xAxis.tickValues()).toEqual(null);
-      expect(nvChart.yAxis.tickValues()).toEqual([]);
-    });
-
-    it("hides all axis ticks if showXAxis(false) and showYAxis(false) were both called", function() {
-      var nvChart = chart.showXAxis(false).showYAxis(false).configure();
-      expect(nvChart.xAxis.tickValues()).toEqual([]);
-      expect(nvChart.yAxis.tickValues()).toEqual([]);
-    });
-  });
-
   describe("showXAxis()", function() {
     it("when called with no arguments, returns true for a new chart", function() {
       expect(chart.showXAxis()).toEqual(true);
@@ -78,6 +51,33 @@ describe("chartBarHorizontal", function() {
 
     it("when called with one argument, returns the chart for method chaining", function() {
       expect(chart.showYAxis(false)).toEqual(chart);
+    });
+  });
+
+  describe("configure()", function() {
+    it("configures the chart to show value labels and axis ticks", function() {
+      var nvChart = chart.configure();
+      expect(nvChart.showValues()).toEqual(true);
+      expect(nvChart.xAxis.tickValues()).toEqual(null); // if null, the D3 axis generates its own tick values
+      expect(nvChart.yAxis.tickValues()).toEqual(null); // if null, the D3 axis generates its own tick values
+    });
+
+    it("hides x axis ticks if showXAxis(false) was called", function() {
+      var nvChart = chart.showXAxis(false).configure();
+      expect(nvChart.xAxis.tickValues()).toEqual([]);
+      expect(nvChart.yAxis.tickValues()).toEqual(null);
+    });
+
+    it("hides y axis ticks if showYAxis(false) was called", function() {
+      var nvChart = chart.showYAxis(false).configure();
+      expect(nvChart.xAxis.tickValues()).toEqual(null);
+      expect(nvChart.yAxis.tickValues()).toEqual([]);
+    });
+
+    it("hides all axis ticks if showXAxis(false) and showYAxis(false) were both called", function() {
+      var nvChart = chart.showXAxis(false).showYAxis(false).configure();
+      expect(nvChart.xAxis.tickValues()).toEqual([]);
+      expect(nvChart.yAxis.tickValues()).toEqual([]);
     });
   });
 });
